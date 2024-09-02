cc63133fec65b56667ba9cb5a7698198e949b09d
Ian Peng <74917296+pengpengyi92@users.noreply.github.com>
2024-09-02 03:36:34 +0100

    Update disorder_negativity_baseline_obc.jl

--- src/plots_code/disorder_negativity_baseline_obc.jl
+++ src/plots_code/disorder_negativity_baseline_obc.jl
@@ -21,22 +21,22 @@ add_shifted_negativity!(results_2000, params_2000_open.L)
 results_2000.l_adj = results_2000.l ./ Ref(2000)
 
 
-results_plt = @df results_2000 scatter(:l_adj, :shifted_negativity, markercolor = :green,
+results_plt = @df results_2000 scatter(:l_adj, :shifted_negativity, markercolor = :yellow,
     markershape = :square, alpha = 0.8, markersize = 5.0,
     label = L"L = 2000", legend = :bottomright, markerstrokewidth = 2, 
-    markerstrokecolor = :green, dpi = 600, 
+    markerstrokecolor = :yellow, dpi = 600, 
     #ylims = (1.0, 2.25),
     xlabel = L"l/L", ylabel = L"\mathcal{E}^s_{A_1:A_2}",
     clip_mode = "individual")
 
 @df results_1000 scatter!(results_plt, :l_adj, :shifted_negativity, color = :blue,
-label = L"L = 1000", alpha = 0.8, markerstrokecolor = :blue, markersize = 5.0,
+label = L"L = 1000", alpha = 0.8, markerstrokecolor = :red, markersize = 5.0,
 clip_mode = "individual")
 
 optim_result = optimize(x -> infinite_chain_negativity_loss(x, results_2000.l, results_2000.negativity), ones(1))
 k = Optim.minimizer(optim_result)[1] - 0.03 #fudge factor
 plot!(l -> infinite_chain_negativity(l, k), 0.0:0.01:0.5, label = "SDRG",
-colour = :red, linestyle = :dashdot)
+colour = :blue, linestyle = :dashdot)
 
 saveplot(results_plt, img_path, "baseline_negativity_plot_obc")
 
